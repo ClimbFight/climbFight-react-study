dde5f05dc8e499c405740df56be02166edb5591d
songmilee <lsm941010@gmail.com>
2021-09-26 10:48:44 +0900

    채팅어플리케이션 - 로그인 화면

---
+++ smlee/7_react-native-simple-chat/src/screens/Login.js
@@ -0,0 +1,71 @@
+import React, { useRef, useState, useEffect } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import styled from "styled-components";
+import { Image, Input, Button } from "../components";
+import { images } from "../utils/images";
+import { validateEmail, removeWhiteSpace } from "../utils/common";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({theme}) => theme.background};
+    padding: 20px;
+    padding-top: ${({insets: {top}}) => top}px;
+    padding-bottom: ${({insets: {bottom}}) => bottom}px;
+`;
+
+const ErrorText = styled.Text`
+    color: ${({theme}) => theme.errorText};
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+`
+
+const Login = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword ] = useState('');
+    const passwordRef = useRef();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [disabled, setDisabled] = useState(true);
+
+    const insets = useSafeAreaInsets();
+
+    useEffect(() => {
+        setDisabled(!(email && password && !errorMessage));
+    }, [email, password, errorMessage]);
+
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhiteSpace(email);
+        setEmail(changedEmail);
+
+        setErrorMessage(validateEmail(changedEmail) ? '' : 'Please verify your email');
+    };
+
+    const _handlePasswordChange = password => {
+        setPassword(removeWhiteSpace(password));
+    };
+
+    const _hanldeLoginButtonPress = () => {
+        print("login button clicked");
+    };
+
+    return(
+        <KeyboardAwareScrollView contentContainerStyle={{flex: 1}} extraScrollHeight={20}>
+            <Container insets={insets}>
+                <Image url={images.logo} imageStyle={{ borderRadius: 8 }}/>      
+                <Input label="Email" value={email} onChangeText={_handleEmailChange} onSubmitEditing={() => passwordRef.current.focus()} placeholder="Email" returnKeyType="next" />      
+                <Input ref={passwordRef} label="Password" value={password} onChangeText={_handlePasswordChange} onSubmitEditing={_hanldeLoginButtonPress} placeholder="Password" returnKeyType="done" isPassword />
+                <ErrorText>{errorMessage}</ErrorText>
+
+                <Button title="Login" onPress={_hanldeLoginButtonPress} disabled={disabled} />
+                <Button title="Sign up with email" onPress={() => navigation.navigate('Signup')} isFilled={false} disabled={false}/>
+            </Container>
+        </KeyboardAwareScrollView>
+    );
+};
+
+export default Login;
---
+++ smlee/7_react-native-simple-chat/src/utils/images.js
@@ -0,0 +1,7 @@
+
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-fc62b.appspot.com/o';
+
+export const images = {
+    logo: `${prefix}/icon.png?alt=media`,
+    photo: `${prefix}/photo.png?alt=media`,
+}
